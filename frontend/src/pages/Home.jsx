23fda9f851429817aee30661806049cfb488c6cd
biraj1551 <vrajvyas2204@gmail.com>
2025-11-11 15:40:37 +0530

    Changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import BentoGrid from "../Components/BentoGrid"
 const Home = () => {
   return (
     <div className='max-w-[90%] mx-auto my-6 md:max-w-[90%] bg-black md:h-[calc(100vh-3rem)] rounded-2xl px-4'>
-      <BentoGrid />
+      <BentoGrid />Hello
     </div>
   )
 }
